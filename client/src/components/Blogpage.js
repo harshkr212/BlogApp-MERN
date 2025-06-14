7984f6e89279fbe2fd5aee55d88a2b3a695619f0
harshkr212 <guptagamer8@gmail.com>
2025-06-14 12:55:13 +0530

    Update Blogpage.js

--- client/src/components/Blogpage.js
+++ client/src/components/Blogpage.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import imageP from './learn-python.jpg'
 import BlogContext from '../Contextblog/BlogContext'
 const Blogpage = (props) => {
+  const host="https://blogapp-backend-8qit.onrender.com"
   const context = useContext(BlogContext);
   const { markPosted, deleteblog } = context;
   const handleClick = (e) => {
@@ -11,7 +12,7 @@ const Blogpage = (props) => {
   return (
     <div>
       <div className="card" style={{ maxWidth: '540px' }}>
-        <img src={`http://localhost:5000/uploads/${props.blog.image}`} className="img-fluid rounded-start" alt="..." />
+        <img src={`${host}/uploads/${props.blog.image}`} className="img-fluid rounded-start" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.blog.title}</h5>
           <p className="card-text">{props.blog.content}</p>
